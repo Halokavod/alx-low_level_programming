7e281bb4063ea6a926e2b340f6373ca2d6f2125b
Halokavod <abelojiamglory@gmail.com>
2022-10-14 09:19:23 -0700

    print the size of computers in C

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -5,11 +5,11 @@
  **/
 int main(void)
 {
-	printf("size of a char:%b byte(s)\n", sizeof(char));
-	printf("size of an int:%b byte(s)\n", sizeof(int));
-	printf("size of a long int:%b byte(s)\n", sizeof(long int));
-	printf("size of a long long int:%b byte(s)\n", sizeof(long long int));
-	printf("size of a float:%b byte(s)\n", sizeof(float));
+	printf("size of a char: %b byte(s)\n", sizeof(char));
+	printf("size of an int: %b byte(s)\n", sizeof(int));
+	printf("size of a long int: %b byte(s)\n", sizeof(long int));
+	printf("size of a long long int: %b byte(s)\n", sizeof(long long int));
+	printf("size of a float: %b byte(s)\n", sizeof(float));
 	return (0);
 }
 
