cddb6c6b6234bf724dbc9a2a37147b543a50b945
Halokavod <abelojiamglory@gmail.com>
2022-10-14 17:07:24 -0700

    print standard error

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,10 +2,14 @@
 #include<unistd.h>
 /**
  * main - Entry point
- * Return: 1 (success)
- **/
+ *
+ * Description: 'and that piece of art is useful" - Dora Korpar, 2015-10-19'
+ *
+ * Return: 0 (success)
+ */
+
 int main(void)
 {
-	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n" 59);
+	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
