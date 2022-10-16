2bb3f1c0482d7e310feb11afdec4106d39a87137
Halokavod <abelojiamglory@gmail.com>
2022-10-16 04:01:08 -0700

    print positive and negative numbers

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -15,20 +15,20 @@ int main(void)
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	printf("%d \n", n);
 	if (n > 0)
 	{
-		printf("%d is positive\n", n);
-	}
 
+		printf("%d is positive\n");
+	}
 	else if (n < 0)
 	{
-		printf("%d is negative\n", n);
+
+		printf("%d is negative\n");
 	}
-	
-	if (n == 0) 
+	else (n == 0)
 	{
-		printf("%d is zero\n", n);
+
+		printf("%d is zero\n");
 	}
 	return (0);
 }
