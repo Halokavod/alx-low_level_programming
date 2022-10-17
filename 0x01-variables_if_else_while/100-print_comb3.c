ffe32e68e25667f74e2cd2f90a033893c46fd4d7
Halokavod <abelojiamglory@gmail.com>
2022-10-17 16:25:19 -0700

    print two digits

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -14,11 +14,12 @@ int main(void)
 	for (a = 0; a <= 9; a++)
 
 	for (b = a + 1; b <= 9; b++)
-
+	
 	{
 		putchar(a + '0');
 		putchar(b + '0');
 	}
+	if (a < 9);
 	{
 		putchar(',');
 		putchar(' ');
