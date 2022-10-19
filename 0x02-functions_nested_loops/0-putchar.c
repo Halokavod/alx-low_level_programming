b893f391963927d5efb8c4d649aec956d787f87b
Halokavod <abelojiamglory@gmail.com>
2022-10-19 16:57:34 -0700

    prints _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include"main.h"
+/**
+ * main - Use local header
+ * Description: prints _putchar.
+ *
+ * Return: 0 (Success)
+ **/
+
+int main(void)
+{
+	char _putchar;
+
+	printf("_putchar\n");
+	return (0);
+}
