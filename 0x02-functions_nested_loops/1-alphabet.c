aefb4d1a41d14e75830f9a553490ccde2b2f9b9f
Halokavod <abelojiamglory@gmail.com>
2022-10-20 14:21:12 -0700

    print aphabets

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -3,18 +3,17 @@
  * main - Alphabets
  * Description: prints the alphabet.
  *
- * Return: 0 (Success)
+ * Return: void
  **/
-
 
 void print_alphabet(void);
 {
-	int y;
+	char y;
 
 	for (y = 'a'; y <= 'z'; y++)
 	{
 		_putchar(y);
 	}
 	_putchar('\n');
-	return (0);
+
 }
