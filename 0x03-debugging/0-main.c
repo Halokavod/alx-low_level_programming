7929fcd277a6f633f3d1bb31252090a351cad52f
Halokavod <abelojiamglory@gmail.com>
2022-10-21 08:32:26 -0700

    print positive/negative

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+
+
+/**
+ *
+ * * main - tests function prints if integer positive or negative
+ *
+ * * Return: 0
+ *
+ * */
+
+
+
+int main(void)
+
+{
+
+	int o;
+
+
+
+	o = 0;
+
+	positive_or_negative(o);
+
+
+	return (0);
+}
---
+++ 0x03-debugging/README.md
@@ -0,0 +1,1 @@
+1.This file test that the function positive_/_negative() gives the correct output when given a case of 0.
