8d9c788de8025a67d9cb76011273ec427c37b57e
Halokavod <abelojiamglory@gmail.com>
2022-10-21 19:24:11 -0700

    print 0 and 9

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -2,12 +2,11 @@
 /**
  * print_numbers -  prints the numbers except 2 and 4.
  */
-
 void print_numbers(void)
 {
-	char a;
+	char c;
 
-	for (a = '0'; a <= '9'; a++)
+	for (c = '0'; c <= '9'; c++)
 	_putchar(c);
 	_putchar('\n');
 }
