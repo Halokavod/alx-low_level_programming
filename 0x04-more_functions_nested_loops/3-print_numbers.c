d26d0617d858dbfa0d6c28349fc5f131368c8088
Halokavod <abelojiamglory@gmail.com>
2022-10-21 19:20:25 -0700

    print 0-9

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * print_numbers -  prints the numbers except 2 and 4.
+ */
+
+void print_numbers(void)
+{
+	char a;
+
+	for (a = '0'; a <= '9'; a++)
+	_putchar(c);
+	_putchar('\n');
+}
