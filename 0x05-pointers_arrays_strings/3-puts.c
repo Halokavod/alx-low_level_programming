715ef87dc75bf27e321d8ae93ef19c7fbe091da6
Halokavod <abelojiamglory@gmail.com>
2022-10-29 08:55:11 -0700

    print a string

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <unistd.h>
+
+/**
+ *  _puts - print a string followed by a new line
+ *
+ *  @str: an inputted string
+ *
+ *  Return: 0 (success)
+ */
+
+void _puts(char *str)
+{
+	int m = 0;
+
+	while (*(str + 1) != '\0')
+	{
+		_putchar(str[m]);
+		m++;
+	}
+	_putchar('\n');
+}
